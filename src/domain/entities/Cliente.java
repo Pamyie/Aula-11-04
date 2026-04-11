da2d42a6b323cfc950474116480c164e721baf7b
Pamyie <pamelamontanari123@gmail.com>
2026-04-11 14:19:33 -0300

    cliente

--- src/domain/entities/Cliente.java
+++ src/domain/entities/Cliente.java
@@ -1,4 +1,28 @@
 package domain.entities;
 
 public class Cliente {
+    private String nome;
+    private String cpf;
+
+
+    public Cliente(String nome, String cpf){
+
+        if (nome == null || nome.trim().isEmpty()){
+            throw new IllegalArgumentException("Nome do clinente é obrigatório.");
+        }
+        if (cpf == null || cpf.trim().isEmpty()) {
+            throw new IllegalArgumentException("CPF do cliente é obrigatório.");
+        }
+        this.nome = nome;
+        this.cpf = cpf;
+
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
 }
