b33ebc9eb06494bf226f6306cca91b4f7005ff69
Pamyie <pamelamontanari123@gmail.com>
2026-04-11 14:43:23 -0300

    Pedido 1; ass: macaco cu preto

--- src/domain/entities/Pedido.java
+++ src/domain/entities/Pedido.java
@@ -1,4 +1,26 @@
 package domain.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
+  private int numeropedido;
+  private Cliente cliente;
+  private List<Produto> produtos;
+  private String statuspedido;
+  private double total;
+
+  public Pedido(int numeropedido, Cliente cliente){
+      if (cliente == null) {
+          throw new IllegalArgumentException("Cliente é obrigatório");
+
+      }
+      this.numeropedido = numeropedido;
+      this.cliente = cliente;
+      this.produtos = new ArrayList<Produto>();
+      this.statuspedido = "Aberto";
+      this.total = 0.0;
+  }
+
+
 }
