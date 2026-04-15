d3c9716dee22a3cd5a15bf8add47ca958dc81a58
Pamyie <pamelamontanari123@gmail.com>
2026-04-15 00:32:00 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import adapters.controllers.ControllerPedido;
+import application.usecase.FazerPedidoUseCase;
+import application.usecase.ListarPedidosUC;
+import infrastructure.repositories.RepositoryMemoryPedido;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        RepositoryMemoryPedido repository = new RepositoryMemoryPedido();
+        FazerPedidoUseCase fazerPedidoUseCase = new FazerPedidoUseCase(repository);
+        ListarPedidosUC listarPedidosUC = new ListarPedidosUC(repository);
+
+        ControllerPedido controller = new ControllerPedido(fazerPedidoUseCase, listarPedidosUC);
+        controller.iniciar();
+    }
+
 
 }
